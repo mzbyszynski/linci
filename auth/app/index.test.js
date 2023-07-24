4dc2386fe7947f0fb0f4f61302c44232afdf7d54
Marc Zbyszynski <mzbyszynski@users.noreply.github.com>
2023-07-23 22:48:37 -0400

    Added supertest and tests of app

---
+++ auth/app/index.test.js
@@ -0,0 +1,106 @@
+import { Pool } from "pg";
+import supertest from "supertest";
+import app from ".";
+import { StatusCodes } from "http-status-codes";
+
+vi.mock("pg", () => {
+  const Pool = vi.fn();
+  Pool.prototype.query = vi.fn();
+  return { Pool };
+});
+
+let pool;
+
+beforeEach(() => {
+  pool = new Pool();
+  vi.stubEnv("ACCESS_TOKEN_KEY", "asdfsab");
+  vi.stubEnv("REFRESH_TOKEN_KEY", "232223rwsfas");
+  vi.stubEnv("ACCESS_TOKEN_EXPIRE", "5m");
+  vi.stubEnv("REFRESH_TOKEN_EXPIRE", "10m");
+});
+
+describe("/login", () => {
+  test("responds to valid requests with expected status code", async () => {
+    const userData = {
+      id: 123,
+      name: "Test Name",
+      email: "user@example.com",
+    };
+    pool.query.mockResolvedValue({ rows: [userData] });
+
+    const response = await supertest(app).post("/login").send({
+      email: "user@example.com",
+      password: "1231231231212",
+    });
+
+    expect(response.status).toEqual(StatusCodes.OK);
+  });
+
+  test("responds to valid requests with expected response body", async () => {
+    const userData = {
+      id: 123,
+      name: "Test Name",
+      email: "user@example.com",
+    };
+    pool.query.mockResolvedValue({ rows: [userData] });
+
+    const response = await supertest(app).post("/login").send({
+      email: "user@example.com",
+      password: "1231231231212",
+    });
+
+    expect(response.body).toEqual({
+      ...userData,
+      accessToken: expect.any(String),
+      refreshToken: expect.any(String),
+    });
+  });
+});
+
+describe("/signup", () => {
+  const validInput = {
+    email: "test@example.com",
+    name: "Test User",
+    password: "23rdsfs asdfsdafas",
+  };
+
+  test("responds to valid requests with expected status code", async () => {
+    pool.query.mockResolvedValue({
+      rows: [
+        {
+          id: 1324,
+          email: validInput.email,
+          name: validInput.name,
+        },
+      ],
+    });
+
+    const response = await supertest(app)
+      .post("/signup")
+      .send({ ...validInput });
+
+    expect(response.status).toEqual(StatusCodes.CREATED);
+  });
+
+  test("responds to valid requests with expected body", async () => {
+    const userData = {
+      id: 1324,
+      email: validInput.email,
+      name: validInput.name,
+    };
+
+    pool.query.mockResolvedValue({
+      rows: [userData],
+    });
+
+    const response = await supertest(app)
+      .post("/signup")
+      .send({ ...validInput });
+
+    expect(response.body).toEqual({
+      ...userData,
+      accessToken: expect.any(String),
+      refreshToken: expect.any(String),
+    });
+  });
+});
